dee447d4f05a4ca6fc1af6d26931a43d64f0a20d
family <family@DESKTOP-SLIMCR2>
2022-08-27 11:48:37 +0900

    プロジェクト ファイルを追加します。

---
+++ MyClockVB/Display.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Display
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.timeNow = New System.Windows.Forms.Label()
+        Me.secTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'timeNow
+        '
+        Me.timeNow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.timeNow.Font = New System.Drawing.Font("MS UI Gothic", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.timeNow.Location = New System.Drawing.Point(0, 0)
+        Me.timeNow.Name = "timeNow"
+        Me.timeNow.Size = New System.Drawing.Size(174, 86)
+        Me.timeNow.TabIndex = 0
+        Me.timeNow.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'secTimer
+        '
+        Me.secTimer.Enabled = True
+        Me.secTimer.Interval = 1000
+        '
+        'Display
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(174, 86)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.timeNow)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Display"
+        Me.ShowIcon = False
+        Me.Text = "MyClock"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents timeNow As Label
+    Friend WithEvents secTimer As Timer
+End Class
---
+++ MyClockVB/Display.vb
@@ -0,0 +1,60 @@
+﻿
+Imports Microsoft.Win32
+Public Class Display
+
+    Dim ET
+    Dim ssreason As Integer = 8
+    Private Sub secTimer_Tick(sender As Object, e As EventArgs) Handles secTimer.Tick
+
+        If ssreason = 8 Then 'lock状態でないとき
+            ET = ET + 1
+            My.Settings.ET = ET
+
+            If My.Settings.DAY.Date <> DateTime.Now.Date Then
+                ET = 0
+                My.Settings.DAY = Date.Now
+            End If
+            My.Settings.Save()
+            SetDisplayTime()
+        End If
+    End Sub
+
+    Private Sub Display_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'sessionswitch reasonで実行ユーザーのlock/unlockを取得可能　ネットにあまり情報がない
+        AddHandler SystemEvents.SessionSwitch, AddressOf SessionSwitch
+
+
+        ET = My.Settings.ET
+        'If My.Settings.DAY.Date <> DateTime.Now.Date Then
+        'ET = 0
+        'End If
+
+        'SetDisplayTime()
+    End Sub
+
+    Private Sub SetDisplayTime()
+        'timeNow.Text = DateTime.Now.ToString("HH時mm分 ss秒")
+        Dim ts As TimeSpan = New TimeSpan(0, 0, ET)
+        Dim Enddot As String
+        If (ET Mod 2) = 0 Then
+            Enddot = "."
+        Else Enddot = " "
+        End If
+        Dim hms As String = ts.ToString("hh\:mm")
+        timeNow.Text = " " & hms & " " & Enddot
+        'My.Settings.DAY = Date.Now
+
+    End Sub
+
+
+    Private Sub SessionSwitch(ByVal sender As Object, ByVal e As SessionSwitchEventArgs)
+        ssreason = e.Reason
+    End Sub
+
+    Private Sub timeNow_Click(sender As Object, e As EventArgs) Handles timeNow.Click
+
+    End Sub
+End Class
+
+
+
